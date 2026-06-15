8ebfa48e7941111364fdda9b7f7e9d44cd1798c2
Danylo Tykhonov <maloydania@gmail.com>
2026-06-15 16:16:13 +0300

    feat: improve UI/UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import useTodos from "./hooks/useTodos";
+import "./App.css";
+
 export default function App(){
     const {todos, input, deleteTodo, addTodo, filter, filteredTodos, toggleTodo, setFilter, setInput} = useTodos();
-
     return(
-        <div>
-
+        <div className="app">
+           <div className="header">
+              <div className="input">
             <input 
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {if(e.key === "Enter"){
+                addTodo();
+            }}}
             />
+        </div>
+  
+            <button onClick={addTodo}>Add</button>
 
-
-            <button onClick={addTodo}>
-                Add
-            </button>
-
-
-            <button onClick={() => setFilter("all")}>All</button>
-            <button onClick={() => setFilter("active")}>Active</button>
-            <button onClick={() => setFilter("completed")}>Completed</button>
-
+        </div>
+           
+            <div className="filter-buttons">
+            <button className={filter === "all" ? "active-filter" : ""}
+            onClick={() => setFilter("all")}>All</button>
+            <button className={filter === "active" ? "active-filter" : ""}
+            onClick={() => setFilter("active")}>Active</button>
+            <button className={filter === "completed" ? "active-filter" : ""}
+            onClick={() => setFilter("completed")}>Completed</button>
+            </div>
+           
+            <div className="todo-list">
             {filteredTodos.map((todo) => (
-                <div key={todo.id}> 
-                {todo.text}
-                
-            <button onClick={() => deleteTodo(todo.id)}>
-                X
-            </button>
-
+                <div key={todo.id} className="todo-item"> 
+                <div className={`todo-text ${todo.completed ? 'completed' : ''}`}>
+                    {todo.text}
+                 </div>
+            <div className="todo-buttons">    
+            <button onClick={() => deleteTodo(todo.id)}>X </button>
             <button onClick={() => toggleTodo(todo.id)}>
                 {todo.completed ? "Undo" : "Done"}
             </button>
+            </div>
                 </div>
             ))}
+            </div>  
         </div>
     )
 }
